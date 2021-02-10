7f8fbd57e9c89016485ba1238447a5bdc1346b71
yannrelave <74868273+yannrelave@users.noreply.github.com>
2021-02-10 12:18:00 +0100

    MAke a tableau with value

--- src/app/invitation/invitation.page.ts
+++ src/app/invitation/invitation.page.ts
@@ -9,6 +9,14 @@ import { AuthenticationService} from '../service/authentication.service';
 })
 export class InvitationPage implements OnInit {
 
+  words: any[] = [];
+  word = '';
+  cpt = 0;
+  object: any[] = [];
+  header;
+  indice = 0;
+  length = 0;
+
   constructor(private auth: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
@@ -25,5 +33,43 @@ export class InvitationPage implements OnInit {
       alert(error);
     });
   }
+
+  public changeListener(event) {
+    let files: FileList;
+    files = event.target.files;
+    if (files && files.length > 0) {
+      const file: File = files.item(0);
+      const reader: FileReader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = (e) => {
+        const csv: string = reader.result as string;
+        for (let i = 0; i <= csv.length; i++) {
+          if (csv[i] !== '\n') {
+            this.word = this.word + csv[i];
+          } else {
+            this.words[this.cpt] = this.word;
+            this.cpt++;
+            this.word = '';
+          }
+        }
+        this.header = this.words[0].split(',');
+        console.log(this.header);
+        this.words.splice(0, 1);
+        for (let i = 0; i < this.words.length; i++) {
+          const obj = {};
+          const currentline = this.words[i].split(',');
+          for (let j = 0; j < this.header.length; j++) {
+            obj[this.header[j]] = currentline[j];
+          }
+          // this.retreiveMail(obj);
+        }
+      };
+    }
+  }
+
+  /* retreiveMail(tabMail) {
+    console.log(tabMail);
+    console.log(tabMail.Nom);
+  } */
 
 }
