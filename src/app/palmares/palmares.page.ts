dd139cdaddaf14862fe144338e105a6f6fa23fa1
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-02-11 11:51:00 +0100

    palmares avancement

--- src/app/palmares/palmares.page.ts
+++ src/app/palmares/palmares.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Category} from '../Interfaces/category';
+import {Oeuvre} from '../Interfaces/oeuvre';
+import {Router} from '@angular/router';
+import {CategorieCRUDService} from '../services/categorie-crud.service';
 
 @Component({
   selector: 'app-palmares',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PalmaresPage implements OnInit {
 
-  constructor() { }
+  private categories: Category[] = [];
+
+  constructor(private router: Router,
+              private categorieService: CategorieCRUDService,
+  ) { }
 
   ngOnInit() {
+    this.getCategories();
   }
 
+  public getStatus() {
+    let test = this.statusService.getStatusList();
+    test.snapshotChanges().subscribe(res => {
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+        this.status = a;
+      });
+    });
+  }
+
+  public getCategories() {
+    let listCategorie = this.categorieService.getCategoryList();
+    listCategorie.snapshotChanges().subscribe(res => {
+      res.forEach(item => {
+        let a = item.payload.toJSON();
+
+        let maCategorie: Category = {
+          name: '',
+          key: '',
+        };
+
+        maCategorie.key = item.key;
+        maCategorie.name = a['name'];
+        this.categories.push(maCategorie);
+      });
+    });
+  }
 }
