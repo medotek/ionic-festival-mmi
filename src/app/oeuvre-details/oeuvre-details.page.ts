11f088241a70d2c01f7a40598e20272e6d571a6b
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-02-11 16:03:35 +0100

    status condition fix + home page debut fix

--- src/app/oeuvre-details/oeuvre-details.page.ts
+++ src/app/oeuvre-details/oeuvre-details.page.ts
@@ -1,84 +1,86 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { DaoService } from './../services/dao.service';
-import { Oeuvre } from 'src/app/Interfaces/oeuvre';
-import { AlertController } from '@ionic/angular';
-import { AuthenticationService } from '../services/authentication.service';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {DaoService} from './../services/dao.service';
+import {Oeuvre} from 'src/app/Interfaces/oeuvre';
+import {AlertController} from '@ionic/angular';
+import {AuthenticationService} from '../services/authentication.service';
+import {Router} from '@angular/router';
 import {StatusCrudService} from '../services/status-crud.service';
 
 @Component({
-  selector: 'app-oeuvre-details',
-  templateUrl: './oeuvre-details.page.html',
-  styleUrls: ['./oeuvre-details.page.scss'],
+    selector: 'app-oeuvre-details',
+    templateUrl: './oeuvre-details.page.html',
+    styleUrls: ['./oeuvre-details.page.scss'],
 })
 export class OeuvreDetailsPage implements OnInit {
 
-  private oeuvre: Oeuvre;
-  private oeuvreKey;
-  status: any;
+    private oeuvre: Oeuvre;
+    private oeuvreKey;
+    status: any;
 
-  constructor(private route: ActivatedRoute,
-    private dao: DaoService,
-              public alertController: AlertController,
-              private auth: AuthenticationService,
-              private statusService: StatusCrudService,
-              private router: Router,) { }
+    constructor(private route: ActivatedRoute,
+                private dao: DaoService,
+                public alertController: AlertController,
+                private auth: AuthenticationService,
+                private statusService: StatusCrudService,
+                private router: Router,) {
+    }
 
-  ngOnInit() {
-     this.getStatus();
-  }
+    ngOnInit() {
+        this.getStatus();
+    }
 
-  public getStatus() {
-    let test = this.statusService.getStatusList();
-    test.snapshotChanges().subscribe(res => {
-        res.forEach(item => {
-            let a = item.payload.toJSON();
-            this.status = a;
-            if (this.status == 'debut') {
-                this.router.navigate(['/']);
-            }
+    public getStatus() {
+        let test = this.statusService.getStatusList();
+        test.snapshotChanges().subscribe(res => {
+            res.forEach(item => {
+                let a = item.payload.toJSON();
+                this.status = a;
+                if (this.status == 'debut') {
+                    this.router.navigate(['/']);
+                }
+            });
         });
-    });
 
-}
-  ionViewWillEnter(){
-    let oeuvreId = this.route.snapshot.paramMap.get('id');
-    this.oeuvreKey = oeuvreId;
-    let results = this.dao.getOeuvre(oeuvreId);
-    results.snapshotChanges().subscribe(res => {
-      let o = res.payload.toJSON();
-      let testOeuvre: Oeuvre = {
-        name: o['name'],
-        auteur: o['auteur'],
-        nbImages: o['auteur'],
-        key: oeuvreId,
-        categoryId: o['categoryId'],
-        url: o['url'],
-        description: o['description'],
-        contributeurs: o['contributeurs'],
-        technique: o['technique'],
-        realisation: o['realisation'],
-        date: o['date'],
-        voteNumber: o['voteNumber']
-      };
-      this.oeuvre = testOeuvre;
-    })
-  }
+    }
 
-  public vote(){
-    console.log(this.oeuvreKey);
-  }
+    ionViewWillEnter() {
+        let oeuvreId = this.route.snapshot.paramMap.get('id');
+        this.oeuvreKey = oeuvreId;
+        let results = this.dao.getOeuvre(oeuvreId);
+        results.snapshotChanges().subscribe(res => {
+            let o = res.payload.toJSON();
+            let testOeuvre: Oeuvre = {
+                name: o['name'],
+                auteur: o['auteur'],
+                nbImages: o['auteur'],
+                key: oeuvreId,
+                categoryId: o['categoryId'],
+                url: o['url'],
+                description: o['description'],
+                contributeurs: o['contributeurs'],
+                technique: o['technique'],
+                realisation: o['realisation'],
+                date: o['date'],
+                voteNumber: o['voteNumber']
+            };
+            this.oeuvre = testOeuvre;
+        });
+    }
 
-  login() {
-    this.router.navigate(['/form-inscription']);
-  }
+    public vote() {
+        console.log(this.oeuvreKey);
+    }
 
-  logout() {
-    this.auth.logout();
-  }
+    login() {
+        this.router.navigate(['/form-inscription']);
+    }
 
-  home() {
-    this.router.navigate(['/']);
-  }
+    logout() {
+        this.auth.logout();
+    }
+
+    home() {
+        this.router.navigate(['/']);
+    }
 }
